c5504959ee9420301f7cef24e33292d35aa7be6d
Hassaan Syed <hmsye@deakin.edu.au>
2025-05-14 14:33:47 +1000

    Revert "Rebase - dev"
    This reverts commit f5e88e6bc5e2fd55ba75b06bbea047fcc467303e.

--- app/src/main/java/com/example/smishingdetectionapp/MainActivity.java
+++ app/src/main/java/com/example/smishingdetectionapp/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends SharedActivity {
             finish();
         });
 
-        Button learnMoreButton = findViewById(R.id.fragment_container);
+        Button learnMoreButton = findViewById(R.id.learn_more_btn);
         learnMoreButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, EducationActivity.class);
             startActivity(intent);
